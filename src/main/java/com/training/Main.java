e06e1eef3c68c633a9683937ddda219d4f63ed1d
Alesia_Skarakhod <Alesia.Skarakhod@wolterskluwer.com>
2023-03-22 15:50:13 +0200

    initial comment

---
+++ src/main/java/com/training/Main.java
@@ -0,0 +1,28 @@
+package com.training;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String topic = "avro-kafka";
+        String kafkaServer = "http://localhost:9092";
+        String schemaRegistry = "http://localhost:8081";
+
+        KafkaAvroProducer kafkaAvroProducer = new KafkaAvroProducer(kafkaServer, schemaRegistry, topic);
+        KafkaAvroConsumer kafkaAvroConsumer = new KafkaAvroConsumer(kafkaServer, schemaRegistry, topic);
+
+        ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
+        try {
+            executor.scheduleAtFixedRate(kafkaAvroProducer::producer, 0, 1, TimeUnit.SECONDS);
+            kafkaAvroProducer.producer();
+            kafkaAvroConsumer.consumer();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        } finally {
+            kafkaAvroProducer.shutDown();
+        }
+    }
+}
